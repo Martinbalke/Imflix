5df70a548859bd4d05e71fbf15a1048669ef7648
Martin Balke <Mrtnbalke@gmail.com>
2019-08-26 18:51:30 -0700

    finished linking setting up the open source IMDB api and did some testing on using the two api's together

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-//NPM package dependencies 
+//NPM PACKAGE IMPORTS 
 const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
-const unirest = require('unirest');
 
-//Local imports
-const netflixSearch = require('./controller/uNoGS-netflix');
+//LOCAL IMPORTS
+//Function for searching through the open source netflix database supply a search criteria and a callback
+const netflixSearch = require('./utils/uNoGS-netflix');
+//Function for searching through the open source IMDB database supply a ID and a callback
+const imdbSearch = require('./utils/tmdb');
 
 //Initialize express and have it listen on port 
 const app = express();
@@ -20,8 +22,10 @@ const publicDirectory = path.join(__dirname + '/public');
 const partials = path.join(__dirname + '/views/partials');
 const views = path.join(__dirname + '/views');
 
+//Declaring the static directory for express to serve
 app.use(express.static(publicDirectory));
 
+//Setting up Handlebars to be the view engine and then pointing towards the views and partials folders for utilization
 app.set('view engine', 'hbs');
 app.set('views', views);
 hbs.registerPartials(partials);
@@ -35,12 +39,16 @@ app.get('/search', (req, res) => {
 	res.render('search');
 });
 
+
+//Takes in the search form criteria and uses that to search the open source netflix db
 app.post('/search', (req, res) => {
 	const search = req.body.search;
-	console.log(search);
+
 	netflixSearch(search, (error, response) => {
 		if (error) return error;
-		res.send(response);
+
+		const imdbid = response.ITEMS[0].imdbid.toString();
+		console.log(imdbid);
 	});
 })
 
