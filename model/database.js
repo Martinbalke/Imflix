0724d3d146e369c711b313c81495efcf7acfdb1f
Martin Balke <Mrtnbalke@gmail.com>
2019-09-08 20:58:35 -0700

    heroku

--- model/database.js
+++ model/database.js
@@ -5,8 +5,5 @@ const { Client } = require('pg');
 
 //SQL Database configuration
 exports.client = new Client({
-	host: process.env.DB_HOST,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DATABASE
+	connectionString: process.env.DATABASE_URL
 });
